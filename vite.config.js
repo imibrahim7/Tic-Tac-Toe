11e136264d1d1c4ef7a9a8ba847b551cb221da3d
Ibrahim <ibrahim26207@gmail.com>
2024-01-30 19:40:44 +0500

    base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://imibrahim7.github.io/Tic-Tac-Toe/',
   plugins: [react()],
 })
